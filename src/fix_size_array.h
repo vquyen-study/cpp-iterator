78d2db4fcac72c18686478904e87131c249a7414
BaTM1234 <trinhmaibabk2012@gmail.com>
2024-12-03 13:41:16 +0700

    add contructor with initialize list for FixedSizeArray

--- src/fix_size_array.h
+++ src/fix_size_array.h
@@ -9,14 +9,15 @@ namespace xns {
 template<typename T>
 class FixedSizeArray{
     using Pointer = T*;
-    private:
 
     private:
         Pointer m_data;
+        Pointer m_end_of_storage;
         size_t m_capacity;
     public:
         typedef xns::__normal_iterator<Pointer, FixedSizeArray> iterator;
-
+        typedef T                       value_type;
+        typedef size_t                  size_type;
         FixedSizeArray(int capacity) : m_capacity(capacity){
             m_data = new T[capacity];
         }
@@ -27,9 +28,18 @@ class FixedSizeArray{
             }
         }
 
+        FixedSizeArray(std::initializer_list<value_type> __l){
+            m_capacity = xns::distance(__l.begin(), __l.end());
+            m_data = new value_type[m_capacity];
+
+            m_end_of_storage = m_data + m_capacity;
+            std::copy_n( __l.begin(), m_capacity, m_data );
+        }
+
         T& operator[](size_t index){
             return m_data[index];
         }
+
         size_t capacity(){
             return m_capacity;
         }
@@ -37,11 +47,11 @@ class FixedSizeArray{
         iterator begin(){
             return iterator(&m_data[0]);
         }
+
         iterator end(){
             return iterator(&m_data[m_capacity]);
         }
-};
-
+    };
 }
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -8,17 +8,29 @@ using namespace std;
 int
 main(int argc, char** argv)
 {
+    vector<int> iv = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    list<int> lt = {1, 2, 3, 4, 5};
+    xns::FixedSizeArray<int> farray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-  vector<int> iv = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-  list<int> lt = {1, 2, 3, 4, 5};
-  xns::FixedSizeArray<int> farray(10);
+    // C loop stype
+    std::cout << "C loop stype" << std::endl;
+    for (size_t index = 0; index < farray.capacity(); ++index){
+        std::cout << farray[index] << std::endl;
+    }
 
-  for (auto it = farray.begin(); it != farray.end(); ++it){
-      std::cout << *it << std::endl;
-  }
-  // auto value = std::distance(farray.begin(), farray.end());
-  // auto value = std::distance(lt.begin(), lt.end());
-  auto value = xns::distance(farray.begin(), farray.end());
-  cout << "distance between iterator: " << value << endl;
-  return 0;
+    // stype loop with iterator
+    std::cout << "Stype Iterator loop" << std::endl;
+    for (auto it = farray.begin(); it != farray.end(); ++it){
+        std::cout << *it << std::endl;
+    }
+
+    // range base for loop
+    std::cout << "Range base for loop" << std::endl;
+    for (auto it : farray){
+        std::cout << it << std::endl;
+    }
+
+    auto value = xns::distance(farray.begin(), farray.end());
+    cout << "distance between iterator: " << value << endl;
+    return 0;
 }
