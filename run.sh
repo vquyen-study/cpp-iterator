40e7d54b607006cb125c346629e0e02def2caeb1
linux <linux@vnpt.vn>
2025-01-10 12:15:14 +0700

    update run.sh + .dir-locals.el

---
+++ run.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/bash
+
+cd src
+
+echo "------------------------ CFG ------------------------"
+cmake -S . -B build
+echo 
+echo 
+echo 
+echo 
+echo 
+
+echo "------------------------ BLD ------------------------"
+cmake --build build
+
+echo 
+echo 
+echo 
+echo 
+echo 
+echo "------------------------ RUN ------------------------"
+
+build/iter
+
+cd -
+
